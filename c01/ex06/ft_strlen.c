46c54080c7c25b125316fa4113d0e79cb095ae3d
weijiangyang <weijiangyang@laposte.net>
2024-12-16 10:44:20 +0100

    ft_strlen

---
+++ c01/ex06/ft_strlen.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 10:41:54 by weijiangyang      #+#    #+#             */
+/*   Updated: 2024/12/16 10:43:56 by weijiangyang     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strlen(char *str);
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_strlen("hello, weijiang");
+	printf ("length of this str is %d", result);
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count ++;
+	}
+	return (count);
+}
