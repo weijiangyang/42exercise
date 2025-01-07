95fc3b63aaba13b82c8875a01d41ddad660d3035
weijiangyang <weijiangyang@laposte.net>
2025-01-07 16:18:19 +0100

    c04-ex03

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 22:08:43 by weijiangyang      #+#    #+#             */
+/*   Updated: 2025/01/07 16:17:49 by weijiangyang     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_atoi(char *str);
+int		is_digit(char letter);
+int		is_negative(char *str);
+
+int	main(void)
+{
+	char	*result;
+
+	result = ft_atoi("- ---+--+1234ab567");
+	printf ("%s", result);
+	return (0);
+}
+
+char	*ft_atoi(char *str)
+{
+	char	dest[1000];
+	int		index;
+	char	*copied;
+	int		negative;
+
+	copied = dest;
+	index = 0;
+	negative = is_negative(str);
+	if (negative)
+	{
+		dest[0] = '-';
+		index++;
+	}
+	while (!is_digit(*str))
+		str++;
+	while (is_digit(*str))
+	{
+		dest[index] = *str;
+		str++;
+		index++;
+	}
+	dest[index] = '\0';
+	return (copied);
+}
+
+int	is_digit(char letter)
+{
+	char	*nbs;
+
+	nbs = "0123456789";
+	while (*nbs)
+	{
+		if (*nbs == letter)
+			return (1);
+		nbs++;
+	}
+	return (0);
+}
+
+int	is_negative(char *str)
+{
+	int	compte;
+
+	compte = 0;
+	while (*str == ' ' || *str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			compte++;
+		str++;
+	}
+	if (compte % 2)
+		return (1);
+	else
+		return (0);
+}
