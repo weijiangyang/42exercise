2d09f8500c84388ffa7b7ac214014ebf85b287ce
weijiangyang <weijiangyang@laposte.net>
2025-01-13 22:43:35 +0100

    c05-ex01

---
+++ c05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 22:42:55 by weijiangyang      #+#    #+#             */
+/*   Updated: 2025/01/13 22:43:16 by weijiangyang     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb);
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_recursive_factorial(6);
+	printf ("%d", result);
+	return (0);
+}
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 1 || nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
