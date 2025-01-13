ee5108c2569960819873f2d5279e6406ef090540
weijiangyang <weijiangyang@laposte.net>
2025-01-13 22:18:49 +0100

    corrige ex00

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,27 +6,27 @@
 /*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 21:54:42 by weijiangyang      #+#    #+#             */
-/*   Updated: 2025/01/13 21:56:43 by weijiangyang     ###   ########.fr       */
+/*   Updated: 2025/01/13 22:18:19 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_iterative_factorial(int nb);
+long long	ft_iterative_factorial(int nb);
 
 int	main(void)
 {
 	int	result;
 
-	result = ft_iterative_factorial(6);
+	result = ft_iterative_factorial(120);
 	printf ("%d", result);
 	return (0);
 }
 
-int	ft_iterative_factorial(int nb)
+long long	ft_iterative_factorial(int nb)
 {
-	int	result;
-	int	index;
+	long long	result;
+	int			index;
 
 	index = 1;
 	result = 1;
