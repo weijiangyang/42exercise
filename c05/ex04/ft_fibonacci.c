8dc356bf6312a20d31653534b4306748430575f4
weijiangyang <weijiangyang@laposte.net>
2025-01-14 11:52:31 +0100

    c05-ex04

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 11:51:49 by weijiangyang      #+#    #+#             */
+/*   Updated: 2025/01/14 11:52:14 by weijiangyang     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_fibonacci(int index);
+
+int	main(void)
+{
+	int	result;
+
+	result = ft_fibonacci(6);
+	printf ("%d\n", result);
+	return (0);
+}
+
+int	ft_fibonacci(int index)
+{
+	int	result;
+
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	if (index > 1)
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
