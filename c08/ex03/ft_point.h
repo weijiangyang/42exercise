6e9f0b2dfdfde7b261dea090e72c03223d3f8094
weijiangyang <weijiangyang@laposte.net>
2025-01-31 17:07:15 +0100

    ft_point.h

---
+++ c08/ex03/ft_point.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 17:04:12 by weijiangyang      #+#    #+#             */
+/*   Updated: 2025/01/31 17:06:58 by weijiangyang     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+#endif
