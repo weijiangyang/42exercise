744d63b48fc7af1502f495fe661e810dd49dbbab
weijiangyang <weijiangyang@laposte.net>
2025-02-03 22:06:49 +0100

    c08-ex05

--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -6,12 +6,14 @@
 /*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 11:40:48 by weijiangyang      #+#    #+#             */
-/*   Updated: 2025/02/03 11:44:29 by weijiangyang     ###   ########.fr       */
+/*   Updated: 2025/02/03 22:05:53 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include "../ex04/ft_stock_str.h"
+#include "../ex04/ft_strs_tab.h"
 
 void	put_str(char *str)
 {
@@ -51,11 +53,38 @@ void	put_nbr(int nbr)
 	}
 }
 
-int	main(void)
+void ft_show_tab(t_stock_str *par)
 {
-	int	nbr;
+	int i;
+	char sign;
 
-	nbr = 0;
-	put_nbr(nbr);
-	return (0);
+	sign = '\n';
+	i = 0;
+	while (par[i].str != "0")
+	{
+		put_str(par[i].str);
+		write (1, &sign, 1);
+		put_nbr(par[i].size);
+		write(1, &sign, 1);
+		put_str(par[i].copy);
+		write(1,&sign, 1);
+		i++;
+	}
+}
+
+int main(void)
+{
+	int ac;
+	char **av;
+	t_stock_str *result;
+
+	av = malloc ((ac + 1) * sizeof(char *));
+        av[0] = "hello";
+        av[1] = "yang";
+        av[2] = "jean";
+        ac = 3;
+        result = ft_strs_to_tab(ac, av);
+	ft_show_tab(result);
+	free(result);	
+	return 0;
 }
