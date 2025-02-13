ad56e0da68c9d2eb9b34f1bdb39a0b2314bd57ae
weijiangyang <weijiangyang@laposte.net>
2025-02-13 10:34:41 +0100

    c05-ex07

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -6,13 +6,13 @@
 /*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:08:44 by weijiangyang      #+#    #+#             */
-/*   Updated: 2025/01/15 11:12:15 by weijiangyang     ###   ########.fr       */
+/*   Updated: 2025/02/13 10:33:52 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_find_next_prime(int nb);
+/*int	ft_find_next_prime(int nb);
 int	ft_is_prime(int nb);
 
 int	main(void)
@@ -22,31 +22,37 @@ int	main(void)
 	result = ft_find_next_prime(-11);
 	printf ("%d\n", result);
 	return (0);
-}
+}*/
+int ft_is_prime(int nb);
 
 int	ft_find_next_prime(int nb)
 {
 	int	index;
 
 	index = 1;
+	if (ft_is_prime(nb))
+		return (nb);
 	while (index)
 	{
 		if (ft_is_prime(nb + index))
 			return (nb + index);
 		index++;
 	}
+	return 0;
 }
 
-int	ft_is_prime(int nb)
+int     ft_is_prime(int nb)
 {
-	int	index;
+        int     index;
 
-	index = 2;
-	while (index <= nb / 2 || index <= -(nb / 2))
-	{
-		if (nb % index == 0)
-			return (0);
-		index ++;
-	}
-	return (1);
+        index = 2;
+        if (nb == 0 || nb == 1|| nb < 0)
+                return (0);
+        while (index <= nb / 2 || index <= -(nb / 2))
+        {
+                if (nb % index == 0)
+                        return (0);
+                index ++;
+        }
+        return (1);
 }
