d88cad91a1db68927ea2de3e38243365169ca6de
weijiangyang <weijiangyang@laposte.net>
2025-02-13 12:05:25 +0100

    c01-ex03

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -6,14 +6,14 @@
 /*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 21:19:06 by weijiangyang      #+#    #+#             */
-/*   Updated: 2024/12/12 21:46:08 by weijiangyang     ###   ########.fr       */
+/*   Updated: 2025/02/13 12:04:04 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_div_mod(int a, int b, int *div, int *mod);
+/*void	ft_div_mod(int a, int b, int *div, int *mod);
 
 int	main(void)
 {
@@ -24,7 +24,7 @@ int	main(void)
 	printf ("the pointer of 2/3 is %p, its value is %d\n", &div, div);
 	printf ("the pointer of module of 2/3 is %p, its value is %d\n", &mod, mod);
 	return (0);
-}
+}*/
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
