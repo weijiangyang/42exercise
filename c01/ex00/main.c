17a445be4219dee431e743201ebcece3fd218aa8
weijiangyang <weijiangyang@laposte.net>
2025-02-13 11:54:01 +0100

    c01-ex00

--- c01/ex00/main.c
+++ c01/ex00/main.c
@@ -1,31 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
+/*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/10 10:22:06 by weijiangyang      #+#    #+#             */
-/*   Updated: 2025/02/13 11:50:55 by weijiangyang     ###   ########.fr       */
+/*   Created: 2019/06/08 20:15:08 by badam             #+#    #+#             */
+/*   Updated: 2025/02/13 11:50:34 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
-/*void	ft_ft(int *nbr);
+void	ft_ft(int *nbr);
 
-int	main(void)
+int		main(void)
 {
-	int nb;
-	nb = 10;
-
-	ft_ft(&nb);
-	return (0);
-}*/
-
-void	ft_ft(int *nbr)
-{
-	*nbr = 42;
-	printf("%d\n", *nbr);
+	int a = 3;
+	ft_ft(&a);
+	if (a == 42)
+	{
+		write(1, "OK\n", 3);
+	}
+	else
+	{
+		write(1, "FAIL\n", 5);
+	}
 }
