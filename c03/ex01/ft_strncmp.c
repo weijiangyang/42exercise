1016799d0d7a2af0245088a565f2a73736840bb8
weijiangyang <weijiangyang@laposte.net>
2025-02-15 22:02:42 +0100

    c03-ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,11 +6,11 @@
 /*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 11:57:18 by weijiangyang      #+#    #+#             */
-/*   Updated: 2025/01/02 10:41:10 by weijiangyang     ###   ########.fr       */
+/*   Updated: 2025/02/15 22:02:01 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <string.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n);
@@ -24,22 +24,20 @@ int	main(char *char1, char *char2)
 	result = ft_strncmp(char1, char2, 1);
 	printf("%d", result);
 	return (result);
-}
+}*/
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	result;
-	int	compte;
+	unsigned int	compte;
 
 	compte = 1;
-	result = 0;
-	while ((*s1 || *s2) && compte <= n)
+	while (*s1 && compte <= n)
 	{
-		if (*s1 != *s2)
+		if (*s1 != *s2 || compte == n)
 			return ((int)(*s1 - *s2));
 		s1++;
 		s2++;
 		compte++;
 	}
-	return (result);
+	return (*s1 - *s2);
 }
