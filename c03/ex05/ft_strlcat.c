dcec008c435075a34f1728a05949a0770e38adea
weijiangyang <weijiangyang@laposte.net>
2025-02-16 21:43:32 +0100

    c03-ex05

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 16:57:16 by weijiangyang      #+#    #+#             */
-/*   Updated: 2025/01/05 17:37:48 by weijiangyang     ###   ########.fr       */
+/*   Updated: 2025/02/16 21:43:03 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_strlcat(char *dest, char *src, unsigned int size);
+/*int	ft_strlcat(char *dest, char *src, unsigned int size);
 
 int	main(void)
 {
@@ -24,24 +24,28 @@ int	main(void)
 	result = ft_strlcat(dest, src, 12);
 	printf("%d", result);
 	return (0);
-}
-
-int	ft_strlcat(char *dest, char *src, unsigned int size)
+}*/
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	compte;
+	unsigned int	len_initial_dest;
+	unsigned int	len_src;
+	unsigned int	i;
+	unsigned int	j;
 
-	compte = 0;
-	while (*dest)
-	{
-		compte++;
-		dest++;
-	}
-	while (*src)
-	{
-		*dest = *src;
-		compte++;
-		dest++;
-		src++;
-	}
-	return (compte);
+	len_initial_dest = 0;
+	len_src = 0;
+	while (dest[len_initial_dest] != '\0')
+		len_initial_dest++;
+	while (src[len_src] != '\0')
+		len_src++;
+	if (size == 0)
+		return (len_src);
+	if (size <= len_initial_dest)
+		return (len_src + size);
+	i = len_initial_dest;
+	j = 0;
+	while (src[j] != '\0' && i < size - 1)
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (len_src + len_initial_dest);
 }
