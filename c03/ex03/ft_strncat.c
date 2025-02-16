4e5e78c8c290c10672b4cf86ab1475e9dca4040b
weijiangyang <weijiangyang@laposte.net>
2025-02-16 18:51:55 +0100

    c03-ex03

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -6,45 +6,29 @@
 /*   By: weijiangyang <weijiangyang@laposte.net>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 15:37:00 by weijiangyang      #+#    #+#             */
-/*   Updated: 2025/01/03 15:42:51 by weijiangyang     ###   ########.fr       */
+/*   Updated: 2025/02/16 18:51:39 by weijiangyang     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-char	*ft_strncat(char *dest, char *src, unsigned int nb);
-
-int	main(void)
-{
-	char	dest[100] = "Hello, ";
-	char	*src;
-	char	*result;
-
-	src = "les gars!";
-	result = ft_strncat(dest, src, 5);
-	printf ("%s", result);
-	return (0);
-}
-
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	int	index;
-	int	i;
+	unsigned int	i;
+	unsigned int	j;
 
-	index = 0;
 	i = 0;
-	while (dest[index])
-	{
-		index++;
-	}
-	while (src[i] && i <= nb - 1)
-	{
-		dest[index] = src[i];
-		index++;
+	while (dest[i] != '\0')
 		i++;
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	if (i == nb)
-		dest[index] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
+
